0db4e3bbd4a5b73ae7b866834c5b5bd56605253b
jamesjdistefano <jamesjdistefano@gmail.com>
2021-09-07 10:44:01 -0400

    Adds Flex test

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,14 +1,5 @@
 <template>
   <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
-    </header>
     <slot/>
   </div>
 </template>
@@ -30,21 +21,12 @@ body {
 }
 
 .layout {
-  max-width: 760px;
+  max-width: 1200px;
   margin: 0 auto;
   padding-left: 20px;
   padding-right: 20px;
 }
 
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
 
-.nav__link {
-  margin-left: 20px;
-}
+
 </style>
---
+++ src/pages/Flex.vue
@@ -0,0 +1,71 @@
+<template>
+  <Layout>
+
+    <ul class = "flextest">
+      <li class = "item">Item 1</li>
+      <li class = "item">Item 2</li>
+      <li class = "item">Item 3</li>
+      <li class = "item big">Item 4</li>
+      <li class = "item">Item 5</li>
+      <li class = "item">Item 6</li>
+    </ul>
+
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+
+.item {
+  background: tomato;
+  width: 100px;
+  height: 100px;
+  margin: 10px;
+
+  color: white;
+  font-weight: regular;
+  font-size: 2em;
+  text-align: center;
+}
+
+.big {
+  width: 166px;
+  height: 166px;
+}
+
+.tile-one {
+  flex-grow: 1;
+}
+
+.tile-two {
+  flex-grow: 2;
+}
+
+.wrap {
+  flex-wrap: wrap;
+
+}
+
+.flextest {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-content: flex-end;
+  padding: 2px;
+  border: 1px blue solid;
+  list-style: none;
+
+}
+
+</style>
